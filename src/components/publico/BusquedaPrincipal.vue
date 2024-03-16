c513fc7fdddbd7d16fd778f9a8b999a88dd3be9c
Luciano <lucho.2012.tandil@gmail.com>
2024-03-16 18:09:38 -0300

    En alquileres se quitan enlaces y opciones que no se usan

--- src/components/publico/BusquedaPrincipal.vue
+++ src/components/publico/BusquedaPrincipal.vue
@@ -44,7 +44,8 @@
                                     <div class="col-12 col-sm product-name-cont">
                                         {{ resultado?.products?.name }}
                                         <small v-if="resultado?.url"><a :href="resultado?.url" target="_blank">IR A WEB</a></small>
-                                        &nbsp;<small class="btn-corregir" @click="corregir_precio(resultado)">CLICK para CORREGIR</small>
+                                        &nbsp;
+                                        <small v-if="resultado?.tipo != 'ALQUILER'" class="btn-corregir" @click="corregir_precio(resultado)">CLICK para CORREGIR</small>
                                     </div>
                                 </div>
                                 
@@ -80,7 +81,7 @@
                                     </div>
                                     
                                     <div class="col-auto">
-                                        <button type="button" class="btn btn-primary" @click="mostrar_estadisticas(resultado)">
+                                        <button v-if="resultado?.tipo != 'ALQUILER'" type="button" class="btn btn-primary" @click="mostrar_estadisticas(resultado)">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-graph-up" viewBox="0 0 16 16">
                                                 <path fill-rule="evenodd" d="M0 0h1v15h15v1H0zm14.817 3.113a.5.5 0 0 1 .07.704l-4.5 5.5a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61 4.15-5.073a.5.5 0 0 1 .704-.07"/>
                                             </svg>
